950a317e06d0ee22182ac46132fee1558fa1cf31
Francisco Cordón <francordon29@gmail.com>
2025-09-28 22:30:21 +0200

    Project change

--- app.py
+++ app.py
@@ -25,11 +25,16 @@ if not os.environ.get("API_KEY"):
 
 
 @app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
+def add_header(response):
+    """
+    Cachea estáticos 1 año y evita recachear HTML dinámico
+    """
+    if response.headers.get("Content-Type", "").startswith("text/html"):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+    else:
+        response.headers["Cache-Control"] = "public, max-age=31536000"
     return response
 
 
